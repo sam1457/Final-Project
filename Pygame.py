e556fff4c33d8e2263330f6c8c39a299d7a90691
sam1457 <165369375+sam1457@users.noreply.github.com>
2024-04-21 18:09:58 -1000

    Created the display window for the game.A minor thing I changed was the name of the game in the title bar.

--- Pygame.py
+++ Pygame.py
@@ -1,5 +1,16 @@
 import pygame
 from sys import exit
-from random import randint, choice
 
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+#img = pygame.image.load('Week8Lab.PNG')    Change the Icon of the tab
+#pygame.display.set_icon(img)
+pygame.display.set_caption('RUN Preacher RUN')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
 
