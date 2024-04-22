8a2178a49b7ab217796b4293ebb915d4930009d5
sam1457 <165369375+sam1457@users.noreply.github.com>
2024-04-21 20:57:04 -1000

    Added text to the middle part of the screen

--- Pygame.py
+++ Pygame.py
@@ -3,13 +3,20 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-#img = pygame.image.load('Week8Lab.PNG')    Change the Icon of the tab
-#pygame.display.set_icon(img)
+
+# img = pygame.image.load('Week8Lab.PNG')    Change the Icon of the tab
+# pygame.display.set_icon(img)
+
 pygame.display.set_caption('RUN Preacher RUN')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 
 sky_surface = pygame.image.load('graphics/Water.JPG')
 ground_surface = pygame.image.load('graphics/Ground.png')
+
+# preacher_Runner = pygame.image.load('graphics/Preacher.jpg')  Create or find a clip art for the main character
+
+text_surface = test_font.render('My Game', False, 'Black')
 
 while True:
     for event in pygame.event.get():
@@ -18,7 +25,8 @@ while True:
             exit()
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 0))
+    screen.blit(text_surface, (300, 50))
 
+    # screen.blit(preacher_Runner, (0, 100))
     pygame.display.update()
     clock.tick(60)
-
