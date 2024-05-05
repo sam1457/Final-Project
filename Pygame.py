d39a17724f75617e55b0fd00bf66a5add06ec7e6
sam1457 <165369375+sam1457@users.noreply.github.com>
2024-05-04 18:28:57 -1000

    *Some of the things that I was able to work on was being able set up rectangles with the characters to then be able to set up the collide function.
    * Was also able convert some of the pictures in the game to be able to fit with Py game and make it run faster.
    
    * The main character is now able to jump up and down.
    
    * I also looked into different ways in drawing lines, polygons and circles. Thinking about including them in the project.

--- Pygame.py
+++ Pygame.py
@@ -1,63 +1,85 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() /1000) - start_time
+    score_surf = test_font.render(f'score: {current_time}',False,(64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf, score_rect)
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-
-# img = pygame.image.load('Week8Lab.PNG')    Change the Icon of the tab
-# pygame.display.set_icon(img)
-
 pygame.display.set_caption('Video Game')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
+game_active = False
+start_time = 0
 
-text_surface = test_font.render('Video Game', False, 'Black')
-score_rect = text_surface.get_rect(center = (400, 50))
 
 sky_surface = pygame.image.load('graphics/Water.JPG')
 sky_surface = pygame.transform.scale(sky_surface, (800, 400))
 ground_surface = pygame.image.load('graphics/Ground.png')
 
-#bird_enemy = pygame.image.load('graphics/bird.png').convert_alpha()
-#bird_rect = bird_enemy.get_rect(topright = (80, 390))
-
 Enemy_surf = pygame.image.load('graphics/Aligator.gif').convert()
-Enemy_rect = Enemy_surf.get_rect(bottomright = (80, 390))
+Enemy_rect = Enemy_surf.get_rect(bottomright=(900, 400))
 
 Stick_Runner = pygame.image.load('graphics/Run.png').convert_alpha()
 player_rect = Stick_Runner.get_rect(midbottom=(80, 380))
 player_gravity = 0
+
+player_stand = pygame.image.load('graphics/Water.jpg')
+player_stand = pygame.transform.scale(player_stand, (800, 400))
+player_stand_rect = player_stand.get_rect(center = (400,200))
+text_surface = test_font.render('Video Game', False, 'Black')
+score_rect = text_surface.get_rect(center=(380, 50))
+
+space_surface = test_font.render('Press Space to Start', False, 'Black')
+space_rect = text_surface.get_rect(center=(340, 350))
+
+person_Runner = pygame.image.load('graphics/Run.png').convert_alpha()
+person_rect = Stick_Runner.get_rect(midbottom=(400, 300))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos):
-                player_gravity = -20
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player_gravity = -20
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 0))
-    screen.blit(text_surface, score_rect)
+        if game_active:
+            if game_active == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                Enemy_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
-    #bird_rect.x -= 4
-    #if bird_rect.right <= 0: Enemy_rect.left = 800
-    #screen.blit(bird_enemy, bird_rect)
+    if game_active:
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 0))
+        display_score()
 
+        Enemy_rect.x -= 4
+        if Enemy_rect.right <= 0: Enemy_rect.left = 800
+        screen.blit(Enemy_surf, Enemy_rect)
 
-    Enemy_rect.x -= 4
-    if Enemy_rect.right <= 0: Enemy_rect.left = 800
-    screen.blit(Enemy_surf, Enemy_rect)
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 380: player_rect.bottom = 380
+        screen.blit(Stick_Runner, player_rect)
 
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 380: player_rect.bottom = 380
-    screen.blit(Stick_Runner, player_rect)
-
+        if Enemy_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('grey')
+        screen.blit(player_stand, player_stand_rect)
+        screen.blit(text_surface, score_rect)
+        screen.blit(space_surface, space_rect)
+        screen.blit(person_Runner, person_rect)
 
 
     pygame.display.update()
